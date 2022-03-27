ac3d0be16595a3107247be79192e72ae28ca870d
Caitano <josiasmartinscaitano@gmail.com>
2022-03-27 17:04:41 -0300

    Adicionado a conversão implícito

---
+++ conversoes.js
@@ -0,0 +1,12 @@
+// tipo de dado
+// booleanos
+
+// conversão implícita
+const numero1 = 456;
+const numeroString = "456a";
+
+// Number();
+// String()
+console.log(numero1 + Number(numeroString))
+
+// conversão explicíta
