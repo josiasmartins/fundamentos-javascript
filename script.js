d5859c4a776dedb79cdfda9b8ac8d56789115142
Caitano <josiasmartinscaitano@gmail.com>
2022-03-27 16:34:22 -0300

    adicionado o boolean

---
+++ script.js
@@ -0,0 +1,39 @@
+// var
+
+// var altura = 5;
+// var width = 7;
+
+// area = altura * width;
+// console.log(area);
+// var area;
+
+// let
+
+// let forma = 'retângulo'
+// let height = 5;
+// let weight = 7;
+// let area;
+
+// if (forma === 'retângulo') {
+//     area = height * weight;
+// } else {
+//     area = (height * weight) / 2;
+// }
+
+// console.log(area);
+
+const forma = 'triangulo';
+const altura = 5;
+const width = 7;
+let area;
+
+if (forma === 'quadrado') {
+    area = altura * width;
+} else {
+    area = (altura * width) / 2;
+}
+
+// const area = altura * width;
+
+console.log(area);
+
