995131a927b41868f0e5af76e3edc3a4849ca3c2
Caitano <josiasmartinscaitano@gmail.com>
2022-03-27 15:14:49 -0300

    exemplos da aula 1

---
+++ type-number.js
@@ -0,0 +1,23 @@
+// type number
+
+const meuNumero = 3;
+
+const num1 = 1;
+const num2 = 2;
+
+const operacaoMatematica = num1 - num2;
+
+console.log(operacaoMatematica);
+
+// ponto flutuante;
+
+const numeroFlutuante = 3.3;
+const pontoFluanteSemZero = .5;
+
+const novaOperacao = num1 / numeroFlutuante;
+console.log(novaOperacao);
+
+// NaN -> Not a Number (não é um número)
+
+const alura = 'ibag';
+console.log(alura * num1)
---
+++ type-string.js
@@ -0,0 +1,11 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = 'senhaSuperSegura';
+const StringDeNumeros = "34567";
+
+const citacao = 'Meu nome é ';
+const meuNome = 'Leonardo';
+// console.log(citacao);
+
+// concatenação (+)
+console.log(citacao + meuNome)
